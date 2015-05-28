50285a5f62c7929fa648a5c283a56d97a7050ed8
Jennifer Mah <jen.mah@hotmail.com>
2015-05-28 18:29:20 +0100

    event listener for submit button is storing the search input value and dropdown pick

--- js/spotify.js
+++ js/spotify.js
@@ -1,4 +1,31 @@
 // https://api.spotify.com/v1/search?q=SOMETHINGHERE&type=SOMETHINGHERE
 $(document).ready(function() {
   
+  // var getSearchItem = $('#search-keyword');
+  // var getSearchType = $('search-type');
+
+  $('#submit_button').on('click', function(event) {
+    search = $('#search-keyword').val();
+    console.log(search);
+    dropdownPick = $('#search-type').val();
+    console.log(dropdownPick);
+  })
 });
+
+
+
+
+
+//   $('#submit_button').on('click', function(event) {
+//     search = $('#search-keyword').val();
+//     console.log(search);
+//     $.ajax({
+//       type: 'GET',
+//       url: 'https://api.spotify.com/v1/search?q=tania%20bowra&type=' + search
+//     })
+//     .done(function(response) {
+//       var searches = [];
+//       debugger;
+//   })
+// });
+// }
