49e68529563e5aa043fd642f545dadf566c38b43
Jennifer Mah <jen.mah@hotmail.com>
2015-05-29 09:37:14 +0100

    API is connected and the function to search artists' albums is working

--- js/spotify.js
+++ js/spotify.js
@@ -1,31 +1,22 @@
 // https://api.spotify.com/v1/search?q=SOMETHINGHERE&type=SOMETHINGHERE
+
 $(document).ready(function() {
-  
-  // var getSearchItem = $('#search-keyword');
-  // var getSearchType = $('search-type');
 
   $('#submit_button').on('click', function(event) {
+    event.preventDefault();
     search = $('#search-keyword').val();
     console.log(search);
     dropdownPick = $('#search-type').val();
     console.log(dropdownPick);
+
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.spotify.com/v1/search?q=' + search +'&type=' + dropdownPick
+    }).done(function(response) {
+      $.each(response.albums.items, function (index, item) {
+        console.log(item);
+      $('#results').append("<li>" + item.name + "</li>")
+    })
+    })
   })
 });
-
-
-
-
-
-//   $('#submit_button').on('click', function(event) {
-//     search = $('#search-keyword').val();
-//     console.log(search);
-//     $.ajax({
-//       type: 'GET',
-//       url: 'https://api.spotify.com/v1/search?q=tania%20bowra&type=' + search
-//     })
-//     .done(function(response) {
-//       var searches = [];
-//       debugger;
-//   })
-// });
-// }
